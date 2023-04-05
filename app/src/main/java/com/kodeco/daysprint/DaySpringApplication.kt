e2a2a4b6219e5977cb982a4398c15512d574c2e2
diayan <diayans@outlook.com>
2023-04-05 17:04:44 +0000

    implement dependency injection

--- app/src/main/java/com/kodeco/daysprint/DaySpringApplication.kt
+++ app/src/main/java/com/kodeco/daysprint/DaySpringApplication.kt
@@ -4,6 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DaySprintApplication : Application() {
-
+class DaySpringApplication : Application() {
+    
 }
---
+++ app/src/main/java/com/kodeco/daysprint/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.kodeco.daysprint.di
+
+import android.app.Application
+import androidx.room.Room
+import com.kodeco.daysprint.db.TaskDatabase
+import com.kodeco.daysprint.db.TaskRepository
+import com.kodeco.daysprint.db.TaskRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TaskDatabase {
+        return Room.databaseBuilder(
+            app,
+            TaskDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TaskDatabase): TaskRepository {
+        return TaskRepositoryImpl(db.dao)
+    }
+}
