ed5b2760f4f3a4748c404124e8275b52c8e06fe6
diayan <diayans@outlook.com>
2023-05-09 07:28:04 +0000

    some more styling

--- app/src/main/java/com/kodeco/daysprint/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/kodeco/daysprint/ui/splash/SplashScreen.kt
@@ -35,19 +35,24 @@
 package com.kodeco.daysprint.ui.splash
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.kodeco.daysprint.R
 import com.kodeco.daysprint.SPLASH_SCREEN
 import com.kodeco.daysprint.TASKS_SCREEN
+import com.kodeco.daysprint.ui.theme.colorAccent
+import com.kodeco.daysprint.ui.theme.colorPrimaryDark
 
 @Composable
 fun SplashScreen(
@@ -59,12 +64,16 @@ fun SplashScreen(
         modifier
             .fillMaxWidth()
             .fillMaxHeight()
-            .background(color = Color.Yellow)
+            .background(color = colorPrimaryDark)
             .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Text(
+            text = "KODECO",
+            style = MaterialTheme.typography.headlineLarge,
+            color = colorAccent
+        )
     }
 
     LaunchedEffect(Unit) {
